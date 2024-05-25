46cc9f73203412576120301ce4654945c8d3f52b
Tashwin <tashwinsjprof@gmail.com>
2024-05-25 17:55:09 +0530

    Update Timeline.jsx

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -3,7 +3,7 @@ export default function Timeline(){
     return(
         <div className="md:text-[18px] sm:text-[15px] sm:p-4 md:p-0 font-normal md:mb-32 sm:mb-8 md:w-[1000px] sm:w-[300px] md:mt-20 sm:mt-12 "> <div className="font-bold text-gray-500 mb-4">Timeline </div>
         <ul>
-            <li><span className="font-bold text-gray-700">April 2024 : </span>Co-authored a Research paper on  "Closed domain question generations using NLP techniques".</li>
+            <li><span className="font-bold text-gray-700">April 2024 : </span>Co-authored a Research paper on  "Closed domain question generation using NLP techniques".</li>
             <li><span className="font-bold text-gray-700">March 2024 : </span>Co-authored a Research paper on "Dynamic Lane Detection with Real-Time Adaptability: Integrating Perspective Transformations, Polynomial Regression, Edge Detection, Gradient Analysis and Object Detection".</li>
             <li><span className="font-bold text-gray-700">Oct 2023 : </span>Interned at <a href="https://pyramiddeveloper.com/"><u className="text-blue-400">Pyramid Developers</u></a>  as a Full stack developer.</li>
             <li><span className="font-bold text-gray-700">July 2023 : </span>1'st team in 🏆 "Intelligent and connected vehicle security hackathon" 
